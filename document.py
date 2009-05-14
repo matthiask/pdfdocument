fd963d0d730813438aacbfebdf42705faaa2f8d6
Matthias Kestenholz <mk@spinlock.ch>
2009-05-14 11:44:15 +0200

    PDFDocument cleanups

--- document.py
+++ document.py
@@ -2,6 +2,7 @@
 
 from reportlab.lib import colors
 from reportlab.lib.enums import TA_LEFT, TA_RIGHT
+from reportlab.lib.fonts import addMapping
 from reportlab.lib.pagesizes import A4, landscape, portrait
 from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
 from reportlab.lib.units import cm, mm
@@ -21,23 +22,81 @@ from django.utils.encoding import force_unicode
 from django.utils.html import escape
 
 
-class Empty(object):
-    pass
-
-
-def save_canvas_state(func, *args, **kwargs):
-    def _func(c, *args, **kwargs):
-        c.saveState()
-        func(c, *args, **kwargs)
-        c.restoreState()
-    return _func
-
-
 pdfmetrics.registerFont(TTFont('ReportingRegular', os.path.join(
     settings.FONT_PATH, settings.FONT_REGULAR)))
 pdfmetrics.registerFont(TTFont('ReportingBold', os.path.join(
     settings.FONT_PATH, settings.FONT_BOLD)))
+addMapping('Reporting', 0, 0, 'ReportingRegular') # regular
+addMapping('Reporting', 0, 1, 'ReportingRegular') # italic
+addMapping('Reporting', 1, 0, 'ReportingBold') # bold
+addMapping('Reporting', 1, 1, 'ReportingBold') # bold & italic
 
+
+class Style(object):
+    _styles = getSampleStyleSheet()
+
+    normal = _styles['Normal']
+    normal.fontName = 'ReportingRegular'
+    normal.fontSize = 8
+    #normal.leading = 2.8*mm
+    #normal.textColor = '#0e2b58'
+
+    heading1 = copy.deepcopy(normal)
+    heading1.fontName = 'ReportingRegular'
+    heading1.fontSize = 18
+    #heading1.leading = 10*mm
+
+    heading2 = copy.deepcopy(normal)
+    heading2.fontName = 'ReportingBold'
+    heading2.fontSize = 14
+    #heading2.leading = 5*mm
+
+    small = copy.deepcopy(normal)
+    small.fontSize = 8
+
+    smaller = copy.deepcopy(normal)
+    smaller.fontSize = 6
+
+    bold = copy.deepcopy(normal)
+    bold.fontName = 'ReportingBold'
+
+    boldr = copy.deepcopy(bold)
+    boldr.alignment = TA_RIGHT
+
+    right = copy.deepcopy(normal)
+    right.alignment = TA_RIGHT
+
+    # alignment = TA_RIGHT
+    # leftIndent = 0.4*cm
+    # spaceBefore = 0
+    # spaceAfter = 0
+
+    tableBase = (
+        ('FONT', (0, 0), (-1, -1), 'ReportingRegular', 8),
+        ('TOPPADDING', (0, 0), (-1, -1), 0),
+        ('BOTTOMPADDING', (0, 0), (-1, -1), 1),
+        ('LEFTPADDING', (0, 0), (-1, -1), 0),
+        ('RIGHTPADDING', (0, 0), (-1, -1), 0),
+        ('VALIGN', (0, 0), (-1, -1), 'TOP'),
+        )
+
+    table = tableBase+(
+        ('ALIGN', (1, 0), (-1, -1), 'RIGHT'),
+        )
+
+    tableLLR = tableBase+(
+        ('ALIGN', (2, 0), (-1, -1), 'RIGHT'),
+        ('VALIGN', (0, 0), (-1, 0), 'BOTTOM'),
+        )
+
+    tableHead = tableBase+(
+        ('FONT', (0, 0), (-1, -1), 'ReportingBold', 8),
+        ('ALIGN', (1, 0), (-1, -1), 'RIGHT'),
+        ('TOPPADDING', (0, 0), (-1, -1), 1),
+        ('BOTTOMPADDING', (0, 0), (-1, -1), 2),
+        ('LINEABOVE', (0, 0), (-1, 0), 0.2, colors.black),
+        ('LINEBELOW', (0, 0), (-1, 0), 0.2, colors.black),
+        )
 
 _Paragraph = Paragraph
 def Paragraph(txt, *args, **kwargs):
@@ -46,9 +105,7 @@ def Paragraph(txt, *args, **kwargs):
 
 
 class PDFDocument(object):
-    style = Empty()
-    story = []
-    show_boundary = False
+    show_boundaries = False
 
     def __init__(self, *args, **kwargs):
         self.doc = BaseDocTemplate(*args, **kwargs)
@@ -58,114 +115,31 @@ class PDFDocument(object):
         self.doc.afterPage=self.__after_page
         self.story = []
 
-    def _process_text(self, text):
-        return force_unicode(escape(text))
+    def init_frames(self):
+        self.frame = Frame(2.6*cm, 2*cm, 16.4*cm, 25*cm, showBoundary=self.show_boundaries)
 
-    def init_styles(self):
-        self._styles = getSampleStyleSheet()
-
-        self.style.normal = self._styles['Normal']
-        self.style.normal.fontName = 'ReportingRegular'
-        self.style.normal.fontSize = 8
-        #self.style.normal.leading = 2.8*mm
-        #self.style.normal.textColor = '#0e2b58'
-
-        self.style.heading1 = copy.deepcopy(self.style.normal)
-        self.style.heading1.fontName = 'ReportingRegular'
-        self.style.heading1.fontSize = 18
-        #self.style.heading1.leading = 10*mm
-
-        self.style.heading2 = copy.deepcopy(self.style.normal)
-        self.style.heading2.fontName = 'ReportingBold'
-        self.style.heading2.fontSize = 14
-        #self.style.heading2.leading = 5*mm
-
-        self.style.small = copy.deepcopy(self.style.normal)
-        self.style.small.fontSize = 8
-
-        self.style.smaller = copy.deepcopy(self.style.normal)
-        self.style.smaller.fontSize = 6
-
-        self.style.bold = copy.deepcopy(self.style.normal)
-        self.style.bold.fontName = 'ReportingBold'
-
-        self.style.boldr = copy.deepcopy(self.style.bold)
-        self.style.boldr.alignment = TA_RIGHT
-
-        self.style.right = copy.deepcopy(self.style.normal)
-        self.style.right.alignment = TA_RIGHT
-
-        # style.alignment = TA_RIGHT
-        # style.leftIndent = 0.4*cm
-        # style.spaceBefore = 0
-        # style.spaceAfter = 0
-
-        self.style.table = (
-            ('FONT', (0, 0), (-1, -1), 'ReportingRegular', 8),
-            ('ALIGN', (1, 0), (-1, -1), 'RIGHT'),
-            ('VALIGN', (0, 0), (-1, -1), 'TOP'),
-            ('TOPPADDING', (0, 0), (-1, -1), 0),
-            ('BOTTOMPADDING', (0, 0), (-1, -1), 0),
-            ('LEFTPADDING', (0, 0), (-1, -1), 0),
-            ('RIGHTPADDING', (0, 0), (-1, -1), 0),
-            )
-
-        self.style.tableLLR = (
-            ('FONT', (0, 0), (-1, -1), 'ReportingRegular', 8),
-            ('ALIGN', (2, 0), (-1, -1), 'RIGHT'),
-            ('VALIGN', (0, 0), (-1, 0), 'BOTTOM'),
-            ('VALIGN', (0, 1), (-1, -1), 'TOP'),
-            ('TOPPADDING', (0, 0), (-1, -1), 0),
-            ('BOTTOMPADDING', (0, 0), (-1, -1), 0),
-            ('LEFTPADDING', (0, 0), (-1, -1), 0),
-            ('RIGHTPADDING', (0, 0), (-1, -1), 0),
-            )
-
-        self.style.tableHead = (
-            ('FONT', (0, 0), (-1, -1), 'ReportingBold', 8),
-            ('ALIGN', (1, 0), (-1, -1), 'RIGHT'),
-            ('VALIGN', (0, 0), (-1, -1), 'TOP'),
-            ('TOPPADDING', (0, 0), (-1, -1), 1),
-            ('BOTTOMPADDING', (0, 0), (-1, -1), 2),
-            ('LEFTPADDING', (0, 0), (-1, -1), 0),
-            ('RIGHTPADDING', (0, 0), (-1, -1), 0),
-            ('LINEABOVE', (0, 0), (-1, 0), 0.2, colors.black),
-            ('LINEBELOW', (0, 0), (-1, 0), 0.2, colors.black),
-            )
-
-    def init_frame(self):
-        self.frame = Frame(2.6*cm, 2*cm, 16.4*cm, 25*cm, showBoundary=self.show_boundary)
-
-    def setup_single_page(self, page_fn, page_fn_later=None):
+    def init_templates(self, page_fn, page_fn_later=None):
         self.doc.addPageTemplates([
             PageTemplate(id='First', frames=[self.frame], onPage=page_fn),
-            PageTemplate(id='Later', frames=[self.frame], onPage=page_fn_later and page_fn_later or page_fn),
+            PageTemplate(id='Later', frames=[self.frame], onPage=page_fn_later or page_fn),
             ])
+        self.story.append(NextPageTemplate('Later'))
 
-    def p(self, text=u'', style=None):
-        if style is None:
-            style = self.style.normal
-        self.story.append(Paragraph(self._process_text(text), style))
+    def _p(default_style):
+        def _fn(self, text=u'', style=None):
+            self.story.append(Paragraph(text), style or default_style)
 
-    def h1(self, text=u''):
-        self.story.append(Paragraph(self._process_text(text), self.style.heading1))
-
-    def h2(self, text=u''):
-        self.story.append(Paragraph(self._process_text(text), self.style.heading2))
-
-    def small(self, text=u''):
-        self.story.append(Paragraph(self._process_text(text), self.style.small))
-
-    def smaller(self, text=u''):
-        self.story.append(Paragraph(self._process_text(text), self.style.smaller))
+    p = _p(Style.normal)
+    h1 = _p(Style.heading1)
+    h2 = _p(Style.heading2)
+    small = _p(Style.small)
+    smaller = _p(Style.smaller)
 
     def spacer(self, height=0.6*cm):
         self.story.append(Spacer(1, height))
 
     def table(self, data, columns, style=None):
-        if style is None:
-            style = self.style.table
-        self.story.append(Table(data, columns, style=style))
+        self.story.append(Table(data, columns, style=style or Style.table))
 
     def hr(self):
         self.story.append(HRFlowable(width='100%', thickness=0.2, color=black))
@@ -218,4 +192,17 @@ class PDFDocument(object):
 
         canvas.drawPath(p, fill=1, stroke=0)
 
+        canvas.restoreState()
+
+    def header(self, canvas, text):
+        canvas.saveState()
+        canvas.setFont('ReportingRegular', 10)
+        canvas.drawString(26*mm, 284*mm, text)
+        canvas.restoreState()
+
+    def footer(self, canvas, texts):
+        canvas.saveState()
+        canvas.setFont('ReportingRegular', 6)
+        for i, text in enumerate(reversed(texts)):
+            canvas.drawString(26*mm, (8+3*i)*mm, text)
         canvas.restoreState()
