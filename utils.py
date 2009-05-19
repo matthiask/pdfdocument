9c086abd428732080257d073bb6b36f04171f7d1
Matthias Kestenholz <mk@spinlock.ch>
2009-05-19 23:14:03 +0200

    Fix date range in worklog_period

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ def worklog_period(obj):
     activity_period = obj.worklogentries.aggregate(Max('date'), Min('date'))
     article_period = obj.articleentries.aggregate(Max('date'), Min('date'))
 
-    min_date = date(1, 1, 1)
+    min_date = date(1900, 1, 1)
     max_date = date(3000, 1, 1)
 
     if not (activity_period['date__min'] or article_period['date__min']):
