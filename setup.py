ed82304692c2e6c224f191bef31d46e2af9c6841
Matthias Kestenholz <mk@spinlock.ch>
2011-03-15 21:16:56 +0100

    PDFDocument 1.0

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pdfdocument',
+      version='1.0',
+      description='Wrapper for ReportLab which allows easy creation of PDF documents.',
+      author='Matthias Kestenholz',
+      author_email='mk@feinheit.ch',
+      url='https://github.com/matthiask/pdfdocument/',
+      packages=['.'],
+      )
