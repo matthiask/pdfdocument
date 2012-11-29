eafc6736010e95d88f3270483e3c0165fd0137e8
Matthias Kestenholz <mk@spinlock.ch>
2012-11-29 17:19:47 +0100

    README -> README.rst

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from distutils.core import setup
 setup(name='pdfdocument',
       version='1.4',
       description='Wrapper for ReportLab which allows easy creation of PDF documents.',
+      long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
       author='Matthias Kestenholz',
       author_email='mk@feinheit.ch',
       url='https://github.com/matthiask/pdfdocument/',
