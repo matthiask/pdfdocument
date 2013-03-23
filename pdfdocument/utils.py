82a08dab6dd763f2d03b0b63151b890b46f07075
Matthias Kestenholz <mk@spinlock.ch>
2013-03-23 16:26:56 +0100

    Remove worklog_period(_string)
    They never belonged into this app anyway.

--- pdfdocument/utils.py
+++ pdfdocument/utils.py
@@ -1,32 +1,8 @@
-from datetime import date
 import re
 
-from django.db.models import Max, Min
 from django.http import HttpResponse
 
 from pdfdocument.document import PDFDocument
-
-
-def worklog_period(obj):
-    activity_period = obj.worklogentries.aggregate(Max('date'), Min('date'))
-    article_period = obj.articleentries.aggregate(Max('date'), Min('date'))
-
-    min_date = date(1900, 1, 1)
-    max_date = date(3000, 1, 1)
-
-    if not (activity_period['date__min'] or article_period['date__min']):
-        return (min_date, max_date)
-
-    start = min(activity_period['date__min'] or max_date, article_period['date__min'] or max_date)
-    end = max(activity_period['date__max'] or min_date, article_period['date__max'] or min_date)
-
-    return (start, end)
-
-
-def worklog_period_string(obj):
-    start, end = obj.worklog_period()
-
-    return u'%s - %s' % (start.strftime('%d.%m.%Y'), end.strftime('%d.%m.%Y'))
 
 
 FILENAME_RE = re.compile(r'[^A-Za-z0-9\-\.]+')
