f9059100080690c3d7af8b03b6672497848b40b7
Matthias Kestenholz <mk@spinlock.ch>
2014-10-21 16:41:22 +0200

    PDFDocument v3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='pdfdocument',
-    version='3.0',
+    version='3.1',
     description='Programmatic wrapper around ReportLab.',
     long_description=open(
         os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
